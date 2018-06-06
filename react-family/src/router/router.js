5113d6cf03b79aaf04c89a11895997fb96f99213
whq361 <whq361@qq.com>
2018-06-06 17:43:31 +0800

    save

--- react-family/src/router/router.js
+++ react-family/src/router/router.js
@@ -1,32 +1,50 @@
-import React from 'react';
-import {AppContainer ,hot } from 'react-hot-loader';
+import React from "react";
+import { hot } from "react-hot-loader";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import Bundle from "./Bundle";
 
-import Home from 'pages/Home/Home';
-import Page1 from 'pages/Page1/Page1';
-import Counter from 'pages/Counter/Counter';
-import UserInfo from 'pages/UserInfo/UserInfo';
-    
-const getRouter = () => (
+import Home from "bundle-loader?lazy&name=home!pages/Home/Home";
+import Page1 from "bundle-loader?lazy&name=page1!pages/Page1/Page1";
+import Counter from "bundle-loader?lazy&name=counter!pages/Counter/Counter";
+import UserInfo from "bundle-loader?lazy&name=userInfo!pages/UserInfo/UserInfo";
 
-    <Router>
-        <div>
-            <ul>
-                <li><Link to="/">首页</Link></li>
-                <li><Link to="/page1">Page1</Link></li>
-                <li><Link to="/counter">Counter</Link></li>
-                <li><Link to="/userinfo">UserInfo</Link></li>
-            </ul>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route path="/page1" component={Page1}/>
-                <Route path="/counter" component={Counter}/>
-                <Route path="/userinfo" component={UserInfo}/>
-            </Switch>
-        </div>
-    </Router>
+const Loading = function() {
+  return <div>Loading...</div>;
+};
+
+const createComponent = component => props => (
+  <Bundle load={component}>
+    {Component => (Component ? <Component {...props} /> : <Loading />)}
+  </Bundle>
 );
 
-// export default getRouter;
-export default hot(module)(getRouter);
+const getRouter = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">首页</Link>
+        </li>
+        <li>
+          <Link to="/page1">Page1</Link>
+        </li>
+        <li>
+          <Link to="/counter">Counter</Link>
+        </li>
+        <li>
+          <Link to="/userinfo">UserInfo</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={createComponent(Home)} />
+        <Route path="/page1" component={createComponent(Page1)} />
+        <Route path="/counter" component={createComponent(Counter)} />
+        <Route path="/userinfo" component={createComponent(UserInfo)} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+// export default hot(module)(getRouter);
+export default getRouter;
--- react-family/webpack.config.js
+++ react-family/webpack.config.js
@@ -1,19 +1,22 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
-  devtool: "inline-source-map",
-  mode: "development",
+  devtool: "cheap-module-source-map",
+  mode: "production",
   /*入口*/
   entry: {
     app: ["react-hot-loader/patch", path.join(__dirname, "src/index.js")],
     vendor: ["react", "react-router-dom", "redux", "react-dom", "react-redux"]
   },
-
   /*输出到dist文件夹，输出文件名字为bundle.js*/
   output: {
     path: path.join(__dirname, "./dist"),
-    filename: "[name].[hash].js",
+    publicPath: "/",
+    filename: "[name].[chunkhash].js",
     chunkFilename: "[name].[chunkhash].js"
   },
   /*src文件夹下面的以.js结尾的文件，要使用babel解析*/
@@ -27,7 +30,10 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
       },
       {
         test: /\.(png|jpg|gif)$/,
@@ -42,14 +48,7 @@ module.exports = {
       }
     ]
   },
-  devServer: {
-    contentBase: path.join(__dirname, "./dist"),
-    historyApiFallback: true,
-    open: true,
-    port: 3000,
-    inline: true,
-    hot: true
-  },
+
   resolve: {
     alias: {
       pages: path.join(__dirname, "src/pages"),
@@ -60,10 +59,40 @@ module.exports = {
     }
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(["dist/*.*"]),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new UglifyJSPlugin(),
+    // new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: path.join(__dirname, "src/index.html")
+    }),
+    new ExtractTextPlugin({
+      filename: "[name].[contenthash:5].css",
+      allChunks: true
+    }),
+    new webpack.optimize.SplitChunksPlugin({
+      chunks: "all",
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      name: true,
+      cacheGroups: {
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        }
+      }
     })
   ]
 };
