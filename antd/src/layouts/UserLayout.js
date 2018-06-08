a22c37eeaed4c59f7b33b991572b568ea9824d4d
whq361@qq.com <whq361@qq.com>
2018-06-08 22:22:29 +0800

    antd-后台

---
+++ antd/src/layouts/UserLayout.js
@@ -0,0 +1,61 @@
+import React, { Fragment } from 'react';
+import { Link, Redirect, Switch, Route } from 'dva/router';
+import DocumentTitle from 'react-document-title';
+import { Icon } from 'antd';
+import GlobalFooter from '../components/GlobalFooter';
+import styles from './UserLayout.less';
+import logo from '../assets/logo.svg';
+import { getRoutes } from '../utils/utils';
+
+
+
+const copyright = (
+  <Fragment>
+    Copyright <Icon type="copyright" />2018 志愿医生出品
+  </Fragment>
+);
+
+class UserLayout extends React.PureComponent {
+  getPageTitle() {
+    const { routerData, location } = this.props;
+    const { pathname } = location;
+    let title = '志愿医生管理系统';
+    if (routerData[pathname] && routerData[pathname].name) {
+      title = `${routerData[pathname].name} - 志愿医生管理系统`;
+    }
+    return title;
+  }
+  render() {
+    const { routerData, match } = this.props;
+    return (
+      <DocumentTitle title={this.getPageTitle()}>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <div className={styles.top}>
+              <div className={styles.header}>
+                <Link to="/">
+                  <img alt="logo" className={styles.logo} src={logo} />
+                  <span className={styles.title}>志愿医生管理系统</span>
+                </Link>
+              </div>
+            </div>
+            <Switch>
+              {getRoutes(match.path, routerData).map(item => (
+                <Route
+                  key={item.key}
+                  path={item.path}
+                  component={item.component}
+                  exact={item.exact}
+                />
+              ))}
+              <Redirect exact from="/user" to="/user/login" />
+            </Switch>
+          </div>
+          <GlobalFooter  copyright={copyright} />
+        </div>
+      </DocumentTitle>
+    );
+  }
+}
+
+export default UserLayout;
---
+++ antd/src/routes/User/Login.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { connect } from 'dva';
+import { Link } from 'dva/router';
+import { Checkbox, Alert, Icon } from 'antd';
+import Login from 'components/Login';
+import styles from './Login.less';
+
+const { Tab, UserName, Password, Mobile, Captcha, Submit } = Login;
+
+@connect(({ login, loading }) => ({
+  login,
+  submitting: loading.effects['login/login'],
+}))
+export default class LoginPage extends Component {
+  state = {
+    type: 'account',
+    autoLogin: true,
+  };
+
+  onTabChange = type => {
+    this.setState({ type });
+  };
+
+  handleSubmit = (err, values) => {
+    const { type } = this.state;
+    if (!err) {
+      this.props.dispatch({
+        type: 'login/login',
+        payload: {
+          ...values,
+          type,
+        },
+      });
+    }
+  };
+
+  changeAutoLogin = e => {
+    this.setState({
+      autoLogin: e.target.checked,
+    });
+  };
+
+  renderMessage = content => {
+    return <Alert style={{ marginBottom: 24 }} message={content} type="error" showIcon />;
+  };
+
+  render() {
+    const { login, submitting } = this.props;
+    const { type } = this.state;
+    return (
+      <div className={styles.main}>
+        <Login defaultActiveKey={type}  onTabChange={this.onTabChange} onSubmit={this.handleSubmit}>
+          <Tab key="account" tab="账户密码登录">
+            {login.status === 'error' &&
+              login.type === 'account' &&
+              !submitting &&
+              this.renderMessage('账户或密码错误（admin/888888）')}
+            <UserName name="userName" placeholder="账户" />
+            <Password name="password" placeholder="密码" />
+          </Tab>
+         
+          <div>
+            
+          </div>
+          <Submit loading={submitting}>登录</Submit>
+         
+        </Login>
+      </div>
+    );
+  }
+}
